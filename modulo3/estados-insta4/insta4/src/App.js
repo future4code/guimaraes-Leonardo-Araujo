6776d28daf239f486cf4f9e90056b258878c21b6
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-02-03 22:17:51 -0300

    entrega exercicio

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -11,16 +11,24 @@ const MainContainer = styled.div`
 
 class App extends React.Component {
   state = {
-    posts: [{nomeUsuario:"Marcos",fotoUsuario:'https://picsum.photos/50/50',fotoPost:'https://picsum.photos/200/150'},{nomeUsuario:'Leonardo',fotoUsuario:'https://picsum.photos/id/50/50',fotoPost:'https://picsum.photos/id/200/150'},{nomeUsuario:'Mari',fotoUsuario:'https://picsum.photos/id/1084/536/354?grayscale/50/50',fotoPost:'https://picsum.photos/seed/picsum/200/150'}]
+    posts: [{nomeUsuario:"Marcos",fotoUsuario:'https://picsum.photos/50/50',fotoPost:'https://picsum.photos/200/150'},{nomeUsuario:'Leonardo',fotoUsuario:'https://picsum.photos/id/50/50',fotoPost:'https://picsum.photos/id/200/150'},{nomeUsuario:'Mari',fotoUsuario:'https://picsum.photos/id/1084/536/354?grayscale/50/50',fotoPost:'https://picsum.photos/seed/picsum/200/150'}],
+    userName:"",
+    userpost:"",
+    userphoto:""
     
   }; 
+  
   render() {
     return (
+      
       <MainContainer>
         {this.state.posts.map((item)=>{
             return <Post nomeUsuario={item.nomeUsuario} fotoUsuario={item.fotoUsuario} fotoPost={item.fotoPost}/>
         })}
-        
+        <input value={this.state.userName} placeholder='Nome'  />
+        <input  value={this.state.userphoto} placeholder='url da foto'/>
+        <input value={this.state.userpost} placeholder='digite post'/>
+        <button>postar </button>
       </MainContainer>
     );
   }
