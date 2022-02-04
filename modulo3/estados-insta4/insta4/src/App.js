cc7dc017d89ede2bc142b41f67e4249ed9fb2e76
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-02-03 21:39:44 -0300

    implementação da parte 1 dos exercicios

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -10,24 +10,17 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+  state = {
+    posts: [{nomeUsuario:"Marcos",fotoUsuario:'https://picsum.photos/50/50',fotoPost:'https://picsum.photos/200/150'},{nomeUsuario:'Leonardo',fotoUsuario:'https://picsum.photos/id/50/50',fotoPost:'https://picsum.photos/id/200/150'},{nomeUsuario:'Mari',fotoUsuario:'https://picsum.photos/id/1084/536/354?grayscale/50/50',fotoPost:'https://picsum.photos/seed/picsum/200/150'}]
+    
+  }; 
   render() {
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'Leonardo'}
-          fotoUsuario={'https://picsum.photos/id/50/50'}
-          fotoPost={'https://picsum.photos/id/200/150'}
-        />
-        <Post
-          nomeUsuario={'Mari'}
-          fotoUsuario={'https://picsum.photos/id/1084/536/354?grayscale/50/50'}
-          fotoPost={'https://picsum.photos/seed/picsum/200/150'}
-        />
+        {this.state.posts.map((item)=>{
+            return <Post nomeUsuario={item.nomeUsuario} fotoUsuario={item.fotoUsuario} fotoPost={item.fotoPost}/>
+        })}
+        
       </MainContainer>
     );
   }
