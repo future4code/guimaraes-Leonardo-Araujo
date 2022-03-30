97223d2d528954f838ccd328a00b93c4e6010636
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-03-29 22:03:56 -0300

    labeX

---
+++ modulo4/projeto-labex/labex/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import react from "react";
+import Button from '@material-ui/core/Button';
+
+export default ()=>{
+
+    return(
+        <>
+        <h1>Labex</h1>
+        <Button variant="contained" color="primary">
+         Hello World
+        </Button>
+
+        </>
+    )
+}
