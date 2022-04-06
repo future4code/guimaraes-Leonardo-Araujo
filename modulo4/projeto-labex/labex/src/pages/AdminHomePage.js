8a34ee4a6aa47d490a1d5c0677aea33c5eae7c98
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-04-05 22:19:43 -0300

    exibir viagens implementado

--- modulo4/projeto-labex/labex/src/pages/AdminHomePage.js
+++ modulo4/projeto-labex/labex/src/pages/AdminHomePage.js
@@ -1,17 +1,36 @@
+
 import axios from "axios";
-import react, { useEffect } from "react";
+import react, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styledComponents from "styled-components";
+const Card=styledComponents.div`
+display: flex;
+justify-content:space-between;
+background-color: red;
+padding: 20px;
+margin: 10px;
+border: 2px solid blue;
+`
 export default ()=>{
     const navigate=useNavigate()
-
+    const token= localStorage.getItem("token")
+    const [viagens,setViagens]=useState([])
     useEffect(()=>{
-        const token= localStorage.getItem("token")
 
         if(token===null){
             navigate('/login')
         }
     },[])
-    
+    useEffect(()=>{
+        const url="https://us-central1-labenu-apis.cloudfunctions.net/labeX/Leonardo-Santos-guimaraes/trips"
+        axios.get(url, {headers:{
+            auth: token}
+        }).then((response)=>{
+                setViagens(response.data.trips)
+        }).catch((error)=>{
+            alert(error.response.data.message)
+        })
+    },[])
 
 
 
@@ -30,7 +49,14 @@ export default ()=>{
         <button onClick={goToBack}>voltar</button>
         <button onClick={goTocreatTrips}>Criar viagem</button>
         <button onClick={goToLogin}>Logout</button>
-        
+        {viagens.map((viagem,i)=>{
+                return(
+                    <Card key={i}>
+                        <h2>{viagem.name}</h2>
+                        <span>X</span>
+                    </Card>
+                )
+        })}
         </>
     )
 }
--- modulo4/projeto-labex/labex/src/pages/TripDetailsPage.js
+++ modulo4/projeto-labex/labex/src/pages/TripDetailsPage.js
@@ -3,12 +3,16 @@ import { useNavigate } from "react-router-dom";
 
 export default ()=>{
     const navigate=useNavigate()
+    const token= localStorage.getItem("token")
     useEffect(()=>{
-        const token= localStorage.getItem("token")
+        
 
         if(token===null){
             navigate('/login')
         }
+    },[])
+    useEffect(()=>{
+
     },[])
 
     return(
