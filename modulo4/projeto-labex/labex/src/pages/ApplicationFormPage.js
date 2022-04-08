2a3327d81aa352c2e87ef9f702dda4604f5bb30c
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-04-07 22:04:48 -0300

    tag form no formulario de increva se

--- modulo4/projeto-labex/labex/src/pages/ApplicationFormPage.js
+++ modulo4/projeto-labex/labex/src/pages/ApplicationFormPage.js
@@ -9,18 +9,20 @@ export default ()=>{
     return(
         <>
         <h2> Inscreva-se para uma viagem</h2>
+        <form onSubmit="">
         <select placeholder="escolha uma viagem">
             <option> Marte</option>
         </select>
-        <input />
-        <input />
-        <input />
-        <input />
-        <select placeholder="escolha país">
+        <input placeholder="nome" required/>
+        <input placeholder="idade"required type="number" />
+        <input placeholder="texto de candidatura" required/>
+        <input  placeholder="profissão"required/>
+        <select placeholder="escolha país required">
             <option> Brasil</option>
         </select>
         <button onClick={goToBack}> Voltar</button>
         <button> Enviar </button>
+        </form>
         </>
     )
 }
