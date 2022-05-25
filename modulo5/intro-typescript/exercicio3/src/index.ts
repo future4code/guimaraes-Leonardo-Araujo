7e0830a7da576971bb356eb4e86bf3c0792b5030
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-05-24 21:30:15 -0300

    implementado exercico 3

--- modulo5/intro-typescript/exercicio3/src/index.ts
+++ modulo5/intro-typescript/exercicio3/src/index.ts
@@ -1,12 +1,6 @@
-//exercicio 1
-
-function checaTriangulo(a:number, b:number, c:number):string {
-    if (a !== b && b !== c) {
-      return "Escaleno";
-    } else if (a === b && b === c) {
-      return "Equilátero";
-    } else {
-      return "Isósceles";
-    }
-  }
-  console.log(checaTriangulo(10,10,10))
+function checaAnoBissexto(ano:number):boolean {
+  const cond1:boolean = ano % 400 === 0
+  const cond2:boolean = (ano % 4 === 0) && (ano % 100 !== 0)
+  return cond1 || cond2
+}
+console.log(checaAnoBissexto(2000))
