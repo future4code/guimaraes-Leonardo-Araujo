2ba12a74e2e8183b49d2b5946c7244b08323859f
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-06-02 19:59:02 -0300

    exercicio 5 implementado

--- modulo5/aprofundamento-express/src/exercicios.ts
+++ modulo5/aprofundamento-express/src/exercicios.ts
@@ -21,22 +21,40 @@ const afazeres: Afazer[] = [{
   id: generateId(),
   title: "Limpar o fogão",
   completed: false
-},{
+}, {
   userId: generateId(),
   id: generateId(),
   title: "Retirar o lixo.",
-  completed:false
+  completed: false
 }]
 
 // exercício 4
-app.get("/todos",(req:Request, res:Response) => {          
-  const tarefasPendentes=afazeres.filter((afazer)=>{
+app.get("/todos", (req: Request, res: Response) => {
+  const tarefasPendentes = afazeres.filter((afazer) => {
     return !afazer.completed
 
   })
   res.status(200).send(tarefasPendentes)
 })
+// exercício 5
+app.post("/todos", (req: Request, res: Response) => {
 
+  try {
+    const authorization = req.headers.authorization
+    if (!authorization)throw new Error("Usuário não encontrado");
+    if(!req.body.title)throw new Error("informe  a tarefa !");
+    const newTarefa:Afazer ={userId:generateId(),id:generateId(),title:req.body.title,completed:req.body.completed}
+    afazeres.push(newTarefa)
+
+    res.status(201).send(afazeres)
+    
+    
+
+}catch (error: any) {
+    res.end(error.message)
+  }
+
+})
 
 
 
