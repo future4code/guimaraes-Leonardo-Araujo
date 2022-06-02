b359bf1fdbf49c4a12a574b08477376a62eb442c
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-06-02 19:16:44 -0300

    exercicio 4 implementado

--- modulo5/aprofundamento-express/src/exercicios.ts
+++ modulo5/aprofundamento-express/src/exercicios.ts
@@ -1,17 +1,41 @@
 import express, { Request, Response } from "express";
 import { AddressInfo } from "net";
-
+import { v4 as generateId } from 'uuid';
 const app = express();
 app.use(express.json());
 // exercício 2
-type Afazer={
-  userId:number,
-  id:number,
-  title:string,
-  completed:boolean
+type Afazer = {
+  userId: string,
+  id: string,
+  title: string,
+  completed: boolean
 }
 // exercício 3
-const afazeres:Afazer[]=[]
+const afazeres: Afazer[] = [{
+  userId: generateId(),
+  id: generateId(),
+  title: "Lavar as louças",
+  completed: true
+}, {
+  userId: generateId(),
+  id: generateId(),
+  title: "Limpar o fogão",
+  completed: false
+},{
+  userId: generateId(),
+  id: generateId(),
+  title: "Retirar o lixo.",
+  completed:false
+}]
+
+// exercício 4
+app.get("/todos",(req:Request, res:Response) => {          
+  const tarefasPendentes=afazeres.filter((afazer)=>{
+    return !afazer.completed
+
+  })
+  res.status(200).send(tarefasPendentes)
+})
 
 
 
