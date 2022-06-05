32cf0b0c7ddf0550a715b151f685aae103fdebe7
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-06-05 16:32:42 -0300

    funcionalidade criar conta implementado

--- modulo5/Labebank/src/index.ts
+++ modulo5/Labebank/src/index.ts
@@ -1,12 +1,31 @@
 import express from "express";
 import { AddressInfo } from "net";
 import cors from 'cors'
-import {users} from './assets/data'
+import { Request, Response } from "express";
+import { users } from './assets/data'
+import { calcularIdade, verificarCpf } from "./functions/functions";
+import { Account } from "./assets/types"
+const usersarray=users
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-console.table(users)
+app.post("/signup", (req: Request, res: Response) => {
+  try {
+    const authorization = req.headers.auth
+    if (!authorization) throw new Error("Usuário não autorizado")
+    const idade = calcularIdade(req.body.birthDate)
+    if(idade<18) throw new Error("Você não possuí a idade mínima para abrir uma conta")
+   const existCpf=verificarCpf(req.body.cpf,users)
+    if(existCpf) throw new Error("CPF já cadastrado !")
+    const account:Account=({...req.body,balance:0,BankStatement:[]})
+    users.push(account)
+    console.log(users)
+    res.status(200).send("Conta criada com sucesso !")
+  } catch (error: any) {
+    res.status(400).end(error.message)
+  }
+})
 
 
 
