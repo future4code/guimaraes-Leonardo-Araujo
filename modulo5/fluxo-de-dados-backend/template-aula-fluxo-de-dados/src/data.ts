c22e4e60caf17aca78f4fa5748c316d7e6f0e9f5
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-06-07 20:12:49 -0300

    exercicio 2 implementado

--- modulo5/fluxo-de-dados-backend/template-aula-fluxo-de-dados/src/data.ts
+++ modulo5/fluxo-de-dados-backend/template-aula-fluxo-de-dados/src/data.ts
@@ -1,85 +1,24 @@
-export const users = [
-    {
-       id: 'juliana-moraes-guimaraes',
-       playlists: [
-          {
-             id: '28654759652',
-             name: 'rock',
-             tracks: [
-                {
-                  id: "4a7-83d62-58h62a5",
-                  name:"pescador-de-ilusoes",
-                  artist: "o rappa",
-                  url: "https://www.youtube.com/watch?=a01QQZy-I"
-                },
-                {
-                   id: '8b4215f1-950b-4c06-bfd6-80c2f8d1bc49',
-                   name: 'Pais e Filhos',
-                   artist: 'Legião Urbana',
-                   url: 'https://www.youtube.com/watch?v=6SWIwW9mg8s'
-                },
-             ],
-          },
-          {
-             id: '28e07ef5-4c6f-4478-9998-1fae4c6d4543',
-             name: 'Pop',
-             tracks: [
-                {
-                   id: '8b4215f1-950b-4c06-bfd6-80c2f8d1cc08',
-                   name: 'Gueto',
-                   artist: 'Iza',
-                   url: 'https://www.youtube.com/watch?v=0dLX40UMUKo',
-                },
-                {
-                   id: '8b4215f1-950b-4c06-bfd6-80c2f8d1bc75',
-                   name: 'Girl From Rio',
-                   artist: 'Anita',
-                   url: 'https://www.youtube.com/watch?v=CuyTC8FLICY',
-                },
-             ],
-          },
-       ],
-    },
-    {
-     id: 'clara-meirelles-guimaraes',
-     playlists: [
-        {
-           id: '285624759654',
-           name: 'rap',
-           tracks: [
-              {
-                 id: '8b4215f1-950b-4c06-bfd6',
-                 name: 'Malvadão',
-                 artist: 'Xamã',
-                 url: 'https://www.youtube.com/watch?v=y5rY2N1XuLI'
-              },
-              {
-               id: '8b4215f1-950b-4c06-bfd6',
-               name: 'Negro Drama',
-               artist: 'Racionais',
-               url: 'https://www.youtube.com/watch?v=y5rY2N1XuLI'
-            }
-           ],
-        },
-        {
-           id: '4c6f-4478-9998-1fae4c6d4543',
-           name: 'Funk',
-           tracks: [
-              {
-                 id: '950b-4c06-bfd6-80c2f8d1cc08',
-                 name: 'Mila',
-                 artist: 'Netinho',
-                 url: 'https://www.youtube.com/watch?v=0dLX40UMUKo',
-              },
-              {
-               id: '950b-4c06-bfd6-80c2f8d1cc08',
-               name: 'Minha pequena Eva',
-               artist: 'Banda Eva',
-               url: 'https://www.youtube.com/watch?v=0dLX40UMUKo',
-            }
-           ],
-        },
-     ],
-  },
-  ];
-  
+import { v4 as generateId } from 'uuid'
+type Produto = {
+   id: string,
+   name: string,
+   price: string
+}
+export const users: Produto[] = [
+   {
+      id: generateId(),
+      name: "Notebook",
+      price: "3599"
+   },
+   {
+      id: generateId(),
+      name: "Televisão",
+      price: "4899"
+   },
+   {
+      id: generateId(),
+      name: "monitor",
+      price: "1428"
+   },
+
+];
