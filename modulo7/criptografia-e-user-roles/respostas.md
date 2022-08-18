54c14f8f83c40a0a8cdf8038149af036b39ce247
Leonardo Santos de Araujo <leosantosdearaujo@gmail.com>
2022-08-18 19:53:09 -0300

    entrega exercicio

---
+++ modulo7/criptografia-e-user-roles/respostas.md
@@ -0,0 +1,13 @@
+### Exercício 01
+A) Round é um fator de segurança do algoritmo , quanto maior o fator maior a segurança e mais o algoritmo demorará para fazer o processamento.
+Salt é uma string aleatória que o algoritmo adiciona ao hash,
+foi utilizado  o valor 12 para round porque para manter o tempo de resposta aceitavel ,12 é o maior valor
+
+### Exercício 02
+
+A) Para executar corretamente os testes o melhor é começar pelo cadastro,porque usuários já existentes não possuem senha criptografada
+
+D) Não é necessário pois o endpoint Profile retorna as informações com base no  ID do usuário  que é recebido através dpo token , informação essa que não é criptografada
+
+
+
---
+++ modulo7/criptografia-e-user-roles/src/data/UserDatabase.ts
@@ -0,0 +1,42 @@
+import { CustomError } from "../error/customError";
+import { user } from "../model/user";
+
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UserDatabase extends BaseDatabase {
+  public createUser = async (user: user) => {
+    try {
+      await UserDatabase.connection
+        .insert({
+          id: user.id,                   
+          email: user.email,
+          password: user.password,
+        })
+        .into("Auth_users");
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+  public async getUserById({id}: any): Promise<any> {
+    const result = await UserDatabase.connection("Auth_users")      
+      .where({ id });
+
+    return result;
+  }
+  
+  public findUser = async (email: string): Promise<user> => {
+    try {
+      const result = await UserDatabase.connection("Auth_users")
+        .select()
+        .where({ email });
+
+      return result[0];
+    } catch (error: any) {
+      throw new CustomError(400, error.message);
+    }
+  };
+  public get=async ():Promise<any>=>{
+    await UserDatabase.connection("Auth_users")
+
+  }
+}
